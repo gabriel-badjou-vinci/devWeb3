0c399fadf4bab71d63db34318521f0d2c6470da2
Gabriel Badjou <gabriel.student.vinci.be>
2026-09-25 17:22:14 +0200

    cors change

--- backend/app.ts
+++ backend/app.ts
@@ -7,7 +7,8 @@ const app = express();
 
 // ... 
 app.use(cors({
-  origin: ['http://localhost:5173', /\.onrender\.com$/],
+  //origin: ['http://localhost:5173', /\.onrender\.com$/],
+  origin: ['https://expenso-frontend-ga7s.onrender.com'],
 }))
 app.use(logger('dev'));
 app.use(express.json());
